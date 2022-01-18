4852dcab9d3d67957a792f324e9def939994f0d6
Victor <publicdomainvic@gmail.com>
2022-01-18 10:37:24 -0400

    Update about

--- BGGfetch/MainForm.cs
+++ BGGfetch/MainForm.cs
@@ -318,7 +318,7 @@ namespace BGGfetch
             var aboutForm = new AboutForm(
                 $"About {programTitle}",
                 $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
-                $"Made for: Mouser{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #16, Week #02 @ January 16, 2022",
+                $"Made for: Mouser{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #18, Week #03 @ January 18, 2022",
                 licenseText,
                 this.Icon.ToBitmap())
             {
@@ -671,7 +671,7 @@ namespace BGGfetch
 
                     List<string> boardgamedesignerList = new List<string>();
 
-                    if (boardgamedesigners.Any())
+                    if (boardgamedesigners != null && boardgamedesigners.Any())
                     {
                         foreach (var designer in boardgamedesigners)
                         {
@@ -686,7 +686,7 @@ namespace BGGfetch
 
                     List<string> boardgamemechanicList = new List<string>();
 
-                    if (boardgamemechanics.Any())
+                    if (boardgamemechanics != null && boardgamemechanics.Any())
                     {
                         foreach (var mechanic in boardgamemechanics)
                         {
